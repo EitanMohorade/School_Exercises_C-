7f40bea07e4fcc3be6b7fa208eda104fb6cdf5f8
Eitan Mohorade <83721060+EitanMohorade@users.noreply.github.com>
2022-09-08 17:22:27 -0300

    Add files via upload
    ejercicio uno hecho y dos semi

---
+++ ejerciciosFISCELLA/ejercicioFISCELLA2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ejercicioFISCELLA2
+{
+    class Program
+    {
+        class Persona
+        {
+            string nombre= "";
+            Int16 edad=0;
+            Int16 DNI;
+            bool sexo=true;
+            double peso=0;
+            double altura=0;
+
+            public Persona()
+            {
+
+            }
+            public Persona(string nombre, Int16 edad, bool sexo)
+            {
+                this.altura = 0;
+                this.DNI = 0;
+                this.edad = edad;
+                this.nombre = nombre;
+                this.peso = 0;
+                this.sexo = sexo;
+            }
+            public Persona(string nombre, Int16 edad, bool sexo, Int16 DNI, double peso, double altura)
+            {
+                this.altura = altura;
+                this.DNI = DNI;
+                this.edad = edad;
+                this.nombre = nombre;
+                this.peso = peso;
+                this.sexo = sexo;
+            }
+            public double calcularMC(double peso)
+            {
+                this.peso = (peso / Math.Pow(this.altura , 2));
+                if(this.peso < 20)
+                {
+                    return -1;
+                }else if (this.peso >= 20 && this.peso <= 25)
+                {
+                    return 0;
+                }else if (this.peso > 25)
+                {
+                    return 1;
+                }
+                return 10;
+            }
+            public bool esMayorDeEdad(Int16 edad)
+            {
+
+                if ()
+                {
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ejerciciosFISCELLA/ejerciciosFISCELLA/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ejerciciosFISCELLA
+{
+    class Program
+    {
+
+        class Cuenta
+        {
+            public String titular { get; set; }
+            public double cantidad { get; set; }
+            public Cuenta(string titular)
+            {
+                this.titular = titular;
+            }
+            public Cuenta(string titular, double cantidad)
+            {
+                this.titular = titular;
+                this.cantidad = cantidad;
+            }
+            public bool ingresar(double plata)
+            {
+                if (plata >= 0) { 
+                   this.cantidad += plata;
+                   return true;
+                }
+                return false;
+            }
+        public bool retirar(double plata)
+            {
+                this.cantidad = this.cantidad - plata;
+                if (this.cantidad < 0)
+                {
+                    this.cantidad = 0;
+                    return true;
+                }
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cuenta cuentarda = new Cuenta("hola", 10);
+            if (cuentarda.ingresar(-10))
+            {
+                Console.WriteLine("se ingreso plata");
+            }
+            else { 
+            Console.WriteLine("no se ingreso nada");
+            }
+            Console.ReadKey();
+        }
+    }
+}
