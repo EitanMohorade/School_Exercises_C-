4d1e928c4fbfa9196403b36e17d3a413113eda71
eitanmohorade <eitanluc@gmail.com>
2022-09-16 08:36:56 -0300

    empezando ejer 3

--- ejerciciosFISCELLA/ejercicioFISCELLA3/Program.cs
+++ ejerciciosFISCELLA/ejercicioFISCELLA3/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ejercicioFISCELLA3
+namespace ejerciciosFISCELLA3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Password
+    {
+        int longitud;
+        int contraseña;
+        public int Longitud { get { return longitud; } set { longitud = value; } }
+        public int Contraseña { get { return contraseña; } set { contraseña = value; } }
+
+        public Password()
+        {
+            this.longitud = 8;
+        }
+        public Password(int longitud)
+        {
+            Random rd = new Random();
+
+            this.longitud = longitud;
+            this.contraseña = rd.Next(0, this.longitud);
         }
     }
 }
