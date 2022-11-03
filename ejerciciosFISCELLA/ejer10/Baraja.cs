34660de5dcf53f636d6b13f7b306bdf1aa23db87
eitanmohorade <eitanluc@gmail.com>
2022-11-03 16:05:14 -0300

    empezar el ejer 10

---
+++ ejerciciosFISCELLA/ejer10/Baraja.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer10
+{
+    internal class Baraja
+    {
+        static Random rnd = new Random();
+        string[] palo = { "espada", "basto", "oro", "copa" };
+        int[] numero = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12 };
+        public List<string> baraja = new List<string>();
+        ///CONSTRUCTORES///
+        public Baraja()
+        {
+            foreach(int numero in numero)
+            {
+                foreach(string str in palo)
+                {
+                    baraja.Add($"{numero} de {str}");
+                }
+            }
+        }
+        ///METODOS///
+        public void barajar()
+        {
+            List<string> barajaPerdida = new List<string>();
+                for (int i = 0; i < baraja.Count; i++)
+                {
+                    int randm = rnd.Next(0, 39);
+                    barajaPerdida.Add(baraja[randm]);
+                }
+                for(int h = 0; h < barajaPerdida.Count; h++)
+                {
+                    baraja.Remove(baraja[h]);
+                    baraja.Add(barajaPerdida[h]);
+                }
+        }
+    }
+}
---
+++ ejerciciosFISCELLA/ejer10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            Baraja baraja = new Baraja();
+            baraja.barajar();
+            foreach(string barja in baraja.baraja)
+            {
+                Console.WriteLine(baraja.baraja[cont]);
+                cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
